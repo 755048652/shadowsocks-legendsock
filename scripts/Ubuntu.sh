449ba18f1872c8da4c5ee24125f0e4b786798cf7
yy tth <755048652@qq.com>
2020-02-02 19:43:08 +0800

    Update Ubuntu.sh

--- scripts/Ubuntu.sh
+++ scripts/Ubuntu.sh
@@ -14,8 +14,8 @@ OLDPATH="$PATH"
 PATH="$PATH:/usr/local/go/bin"
 
 echo "Get source code"
-go get -d -u -v github.com/shadowsocks-server/shadowsocks-legendsock
-cd ~/go/src/github.com/shadowsocks-server/shadowsocks-legendsock
+go get -d -u -v github.com/755048652/shadowsocks-legendsock
+cd ~/go/src/github.com/755048652/shadowsocks-legendsock
 
 echo "Compiling"
 go build -ldflags "-w -s" -o shadowsocks-legendsock
