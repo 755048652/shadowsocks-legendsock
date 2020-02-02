6b1703b974b849a075b0646c71155eb288235b93
yy tth <755048652@qq.com>
2020-02-02 18:59:01 +0800

    Update cipher.go

--- core/cipher.go
+++ core/cipher.go
@@ -6,8 +6,8 @@ import (
 	"net"
 	"strings"
 
-	"github.com/shadowsocks-server/shadowsocks-legendsock/shadowaead"
-	"github.com/shadowsocks-server/shadowsocks-legendsock/shadowstream"
+	"github.com/755048652/shadowsocks-legendsock/shadowaead"
+	"github.com/755048652/shadowsocks-legendsock/shadowstream"
 )
 
 type Cipher interface {
